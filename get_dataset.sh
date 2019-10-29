229a5a0dc7e2731de3f6ab08a3c8c454d8c141c7
Mike Lee <mike8411251995@yahoo.com.tw>
2019-10-29 15:48:07 +0800

    Initialize dependabot/pip/pillow-6.2.0

---
+++ get_dataset.sh
@@ -0,0 +1,11 @@
+# Download dataset from Dropbox
+# wget https://www.dropbox.com/s/7wnulnv1y1s67qr/hw2_train_val.zip?dl=1
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1wFDqKvaV7kmdlJMuhM2VOq4kQzVV5AIy' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1wFDqKvaV7kmdlJMuhM2VOq4kQzVV5AIy" -O hw2_train_val.zip && rm -rf /tmp/cookies.txt
+# Rename the downloaded zip file
+# mv ./hw2_train_val.zip?dl=1 ./hw2_train_val.zip
+
+# Unzip the downloaded zip file
+unzip ./hw2_train_val.zip
+
+# Remove the downloaded zip file
+rm ./hw2_train_val.zip
