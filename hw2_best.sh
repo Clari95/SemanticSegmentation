db396e7cb9fe1f268fc9b3bdbd777202c5b4dbb4
clari <hamann.clarissa@outlook.de>
2019-11-04 19:37:43 +0100

    Final hw2

--- hw2_best.sh
+++ hw2_best.sh
@@ -1,5 +1,5 @@
-# TODO: create shell script for running the testing code of your improved model
-
-RESUME='./log/model_best.pth.tar'
+# TODO: create shell script for running the testing code of the baseline model
+wget https://www.dropbox.com/s/zjzwplw4wr9mvx2/model_best_best.pth.tar?dl=1
+RESUME='model_best_best.pth.tar?dl=1'
 python test.py --resume $RESUME --data_dir $1 --save_dir $2
 
